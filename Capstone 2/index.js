e22c000420c7fc5dc8b9defe7141baa907907cef
SpiSpa <133459316+SpiSpa@users.noreply.github.com>
2025-03-12 10:17:08 -0500

    reorganized the index page

--- Capstone 2/index.js
+++ Capstone 2/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import e from 'express';
 
 const app = express();
 const port = 3000;
@@ -13,6 +12,20 @@ app.get("/", (req, res) => {
 
 app.get("/new-post", (req, res) => {
     res.render("new-post.ejs");
+});
+
+// can you do a loop here to get each post?  or a placeholder for all of them?
+// will need to check to make sure that it exists.
+app.get("/sample-post1", (req, res) => {
+    res.render("sample-post1.ejs");
+});
+
+app.get("/sample-post2", (req, res) => {
+    res.render("sample-post2.ejs");
+});
+
+app.get("/sample-post3", (req, res) => {
+    res.render("sample-post3.ejs");
 });
 
 app.listen(port, () => {
